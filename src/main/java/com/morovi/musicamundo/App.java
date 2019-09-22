253395dd9285365594db2f8cbf7852efad3a745c
Moha <mohamed.rios@mercadolibre.com>
2019-09-22 12:10:07 -0300

    mvn project

---
+++ src/main/java/com/morovi/musicamundo/App.java
@@ -0,0 +1,13 @@
+package com.morovi.musicamundo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
